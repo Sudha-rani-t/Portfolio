9237635532aceba03e6317e39b8dc6685d9f8777
sudha-rain-t <sudharanit10@gmail.com>
2024-12-12 13:31:16 +0530

    changes in experience section 2

--- src/App.js
+++ src/App.js
@@ -598,50 +598,62 @@ const App = () => {
                 }}
               >
                 {experiences.map((experience, index) => (
-                  <Box
-                    key={index}
-                    sx={{
-                      display: "flex",
-                      alignItems: "center",
-                      marginBottom: "40px",
-                      position: "relative",
-                    }}
-                  >
-                    {/* Image on the left */}
-                    <Box
-                      sx={{
-                        width: "150px",
-                        height: "100px",
-                        marginRight: "20px",
-                      }}
-                    >
-                      <img
-                        src={experience.companyImage}
-                        alt={`${experience.title} logo`}
-                        style={{
-                          width: "100%",
-                          height: "150%",
-                          objectFit: "contain",
-                          borderRadius: "5px",
-                        }}
-                      />
-                    </Box>
+        <Box
+          key={index}
+          sx={{
+            display: "flex",
+            flexDirection: {
+              xs: "column", // mobile first (small screens)
+              sm: "row",    // for small screens and up
+            },
+            alignItems: {
+              xs: "center", // center on small screens
+              sm: "flex-start", // left align for larger screens
+            },
+            marginBottom: "40px",
+            position: "relative",
+            textAlign: {
+              xs: "center", // center the text on small screens
+              sm: "left",    // left align for larger screens
+            },
+          }}
+        >
+          {/* Image */}
+          <Box
+            sx={{
+              width: "150px",
+              height: "100px",
+              marginRight: {
+                xs: 0,   // no margin on small screens
+                sm: "20px", // 20px margin on larger screens
+              },
+              marginBottom: {
+                xs: "20px", // margin on bottom for small screens
+                sm: 0,      // no margin on bottom for larger screens
+              },
+            }}
+          >
+            <img
+              src={experience.companyImage}
+              alt={`${experience.title} logo`}
+              style={{
+                width: "100%",
+                height: "150%",
+                objectFit: "contain",
+                borderRadius: "5px",
+              }}
+            />
+          </Box>
 
-                    {/* Card content */}
-                    <CardContent sx={{ flex: 1, marginTop: "20px" }}>
-                      <Typography variant="h5">{experience.title}</Typography>
-                      <Typography variant="body1">
-                        {experience.designation}
-                      </Typography>
-                      <Typography variant="body1">
-                        {experience.years}
-                      </Typography>
-                      <Typography variant="body1">
-                        {experience.duration}
-                      </Typography>
-                    </CardContent>
-                  </Box>
-                ))}
+          {/* Card content */}
+          <CardContent sx={{ flex: 1, marginTop: "20px" }}>
+            <Typography variant="h5">{experience.title}</Typography>
+            <Typography variant="body1">{experience.designation}</Typography>
+            <Typography variant="body1">{experience.years}</Typography>
+            <Typography variant="body1">{experience.duration}</Typography>
+          </CardContent>
+        </Box>
+      ))}
                 <VerticalTimeline>
                   <VerticalTimelineElement
                     className="vertical-timeline-element--work"
